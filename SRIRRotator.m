2bdf9e93e3a0733d1de5508f3c619af57fac45d4
Jacob Cooper <jacob.cooper@york.ac.uk>
2023-09-26 13:54:49 +0100

    Code converted into functions

--- SRIRRotator.m
+++ SRIRRotator.m
@@ -1,6 +1,37 @@
-function [outputArg1,outputArg2] = SRIRRotator(inputArg1,inputArg2)
+function [outputArg1,outputArg2] = SRIRRotator(inputPath,outputPath, yaw, pitch, roll)
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+    HOALibraryPath = 'externals\Higher-Order-Ambisonics';
+    SHTLibraryPath = 'externals\Spherical-Harmonic-Transform';
+    
+    addpath(HOALibraryPath);
+    addpath(SHTLibraryPath);
+    
+    mkdir(outputPath);
+
+    % Place all .wav files in structs
+    fileStruct = dir(fullfile(inputPath,'*.wav'));
+
+    tic
+
+    parfor i = 1:numel(fileStruct)
+    
+        [rawIR, Fs] = audioread(strcat(inputPath, '\', fileStruct(i).name));
+    
+        % Convert SRIRs from SN3D to N3D for rotation
+        N3D = convert_N3D_SN3D(rawIR, 'sn2n');
+        
+        % Rotate SRIRs by the required amount
+        rotated = rotateHOA_N3D(N3D, yaw, pitch, roll);
+        
+        % Convret SRIRs from N3D to SN3D post-rotation
+        SN3D = convert_N3D_SN3D(rotated, 'n2sn');
+    
+        audiowrite( strcat(outputPath, '\', fileStruct(i).name), SN3D, ...
+                    Fs, 'BitsPerSample', 24);
+
+    end
+
+    toc
+
 end
--- runRotations.m
+++ runRotations.m
@@ -1,43 +1,25 @@
-HOALibraryPath = 'externals\Higher-Order-Ambisonics';
-S
+% Paths for MV4
+MV4_inputPath = 'D:\_RESOURCE\MaidaVale-IRs\230907-Zenodo-MV4_5\MV4\AS2\AS2\TOA';
+MV4_rotatedPath = ['D:\_RESOURCE\MaidaVale-IRs\230925_1-Rotation-Compensated-MV4_5' ...
+        '\MV4\AS2\AS2\TOA'];
 
-addpath(HOALibraryPath);
-
-inputPath = 'D:\_RESOURCE\MaidaVale-IRs\230907-Zenodo-MV4_5\MV4\AS2\AS2\TOA';
-rotatedPath = ['D:\_RESOURCE\MaidaVale-IRs\230925_1-Rotation-Compensated-MV4_5' ...
-    '\MV4\AS2\AS2\TOA'];
-
-% Place all .wav files in structs
-fileStruct = dir(fullfile(inputPath,'*.wav'));
-
-% Requried rotations for MV4
+% Required rotations for MV4
 MV4_yaw = 0;
 MV4_pitch = 0;
 MV4_roll = 180;
 
+% MV4 Rotation
+SRIRRotator(MV4_inputPath, MV4_rotatedPath, MV4_yaw, MV4_pitch, MV4_roll);
+
+% Paths for MV5
+MV5_inputPath = 'D:\_RESOURCE\MaidaVale-IRs\230907-Zenodo-MV4_5\MV5\AS1\AS1\TOA';
+MV5_rotatedPath = ['D:\_RESOURCE\MaidaVale-IRs\230925_1-Rotation-Compensated-MV4_5' ...
+        '\MV5\AS1\AS1\TOA'];
+
+% Required rotations for MV4
 MV5_yaw = 0;
 MV5_pitch = 90;
 MV5_roll = 180;
 
-i = 1;
-
-Fs = 48000;
-
-% for i = 1:numel(wavlist)
-
-    % Convert SRIRs from SN3D to N3D for rotation
-    N3D = convert_N3D_SN3D(fileStruct(i).name, 'sn2n');
-    
-    % Rotate SRIRs by the required amount
-    rotated = rotateHOA_N3D(N3D, MV4_yaw, MV4_pitch, MV4_roll);
-    
-    % Convret SRIRs from N3D to SN3D post-rotation
-    SN3D = convert_N3D_SN3D(rotated, 'n2sn');
-
-    audiowrite( strcat(rotatedPath, fileStruct(i).name), SN3D, ...
-                Fs, 'BitsPerSample', 24);
-
-% end
-    
-% end
-
+% MV5 Rotation
+SRIRRotator(MV5_inputPath, MV5_rotatedPath, MV5_yaw, MV5_pitch, MV5_roll);
