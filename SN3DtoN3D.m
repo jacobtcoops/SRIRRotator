f775cfac9480f0a0c4a7a9e6eeec99558bbc7f49
Jacob Cooper <jacob.cooper@york.ac.uk>
2023-09-27 15:04:07 +0100

    26-09 Edits
    Note: Clipping needs fixing (i.e. add normalisation)

--- SN3DtoN3D.m
+++ SN3DtoN3D.m
@@ -1,13 +1,10 @@
-function SRIRRotator(inputPath,outputPath, yaw, pitch, roll)
-%SRIRRotator rotates SRIRs by given yaw, pitch and roll values
+function SN3DtoN3D(inputPath,outputPath)
+%SRIRRotator converts SRIRs from SN3D to N3D
 %   function takes a set of SRIRs, rotates them by the input yaw, pitch and
 %   roll values (in the order given) and saves them
 %   INPUTS
 %       inputFilePath   directory for input SRIRs
 %       outputFilePath  directory for output SRIRs
-%       yaw             yaw rotation (degrees)
-%       pitch           pitch rotation (degrees)
-%       roll            roll rotation (degrees)
 
     % paths for externals
     HOALibraryPath = 'externals\Higher-Order-Ambisonics';
@@ -26,21 +23,15 @@ function SRIRRotator(inputPath,outputPath, yaw, pitch, roll)
     tic
 
     parfor i = 1:numel(SRIRs)
-    
+
         % read in SRIR
         [rawIR, Fs] = audioread(strcat(inputPath, '\', SRIRs(i).name));
     
         % convert SRIRs from SN3D to N3D
         N3D = convert_N3D_SN3D(rawIR, 'sn2n');
-        
-        % rotate SRIRs by the required amount
-        rotated = rotateHOA_N3D(N3D, yaw, pitch, roll);
-        
-        % convert SRIRs from N3D to SN3D post-rotation
-        SN3D = convert_N3D_SN3D(rotated, 'n2sn');
     
         % write to file
-        audiowrite( strcat(outputPath, '\', SRIRs(i).name), SN3D, ...
+        audiowrite( strcat(outputPath, '\', SRIRs(i).name), N3D, ...
                     Fs, 'BitsPerSample', 24);
 
     end
---
+++ runConversion.m
@@ -0,0 +1,17 @@
+% Paths for MV4
+MV4_rotatedPath = ['D:\_RESOURCE\MaidaVale-IRs\230925_1-Rotation-Compensated-MV4_5' ...
+        '\MV4\AS2\AS2\TOA'];
+MV4_convertedPath = ['D:\_RESOURCE\MaidaVale-IRs\230925_2-N3D-All' ...
+        '\MV4\AS2\AS2\TOA'];
+
+% MV4 Conversion
+SN3DtoN3D(MV4_rotatedPath, MV4_convertedPath);
+
+% Paths for MV5
+MV5_rotatedPath = ['D:\_RESOURCE\MaidaVale-IRs\230925_1-Rotation-Compensated-MV4_5' ...
+        '\MV5\AS1\AS1\TOA'];
+MV5_convertedPath = ['D:\_RESOURCE\MaidaVale-IRs\230925_2-N3D-All' ...
+        '\MV5\AS1\AS1\TOA'];
+
+% MV5 Rotation
+SN3DtoN3D(MV5_rotatedPath, MV5_convertedPath);
